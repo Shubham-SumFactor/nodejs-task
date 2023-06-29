fb99cd82a8e4902d630e282958c01119a75626a5
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-29 16:53:05 +0530

    DB connected

---
+++ src/Database/connectDatabase.ts
@@ -0,0 +1,23 @@
+import mysql from 'mysql';
+
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "shubham123",
+    database: "nodetaskdb"
+})
+
+export const connectNodeDatabase = () => {
+    return new Promise((resolve, reject) => {
+
+        try{
+            connection.connect((error) =>{
+                if (error) return reject(error);
+                return resolve("DB connected successfully")
+            })
+        }catch(error){
+            console.log("error connecting DB", error)
+
+        }
+    })
+} 
